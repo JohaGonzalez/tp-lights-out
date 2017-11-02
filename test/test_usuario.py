a80a1fde28c95afa32099e8688eb8c87ad320ed2
JohaGonzalez <joha18_29@hotmail.com>
2017-11-02 20:09:00 -0300

    Agregue una prueba. Cree un modulo usuario con la funcion saludar.

---
+++ test/test_usuario.py
@@ -0,0 +1,17 @@
+#Aca voy a realizar una prueba usando el modulo unittest
+
+import unittest
+import usuario
+
+class UsuarioTest (unittest.TestCase):
+    def testSaludarDeberiaDevolverHola(self):
+
+        #ARRANGE
+        saludoEsperado = "Hola"
+
+        #ACT
+        saludoRecibido = usuario.saludar()
+
+        #ASSERT
+        self.assertEqual(saludoEsperado, saludoRecibido)
+
